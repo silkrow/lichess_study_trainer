5047ea85daa0d8c48f6a70d81890958eb0f04ca8
silkrow <si1krow@outlook.com>
2023-10-03 17:25:44 +0800

    updates

--- au.py
+++ au.py
@@ -1,15 +1,19 @@
 import requests
 
 class Study:
-    def __init__(self, personal_token):
+    def __init__(self, personal_token=None):
         self.base_url = "https://lichess.org/api/"
         self.headers = {
-            "Authorization": f"Bearer {personal_token}",
             "Accept": "application/json",
         }
+        if personal_token:
+            self.set_personal_token(personal_token)
+
+    def set_personal_token(self, personal_token):
+        self.headers["Authorization"] = f"Bearer {personal_token}"
 
     def list_studies(self, username):
-        studies_url = f"{self.base_url}study/by/{username}"  
+        studies_url = f"{self.base_url}study/by/{username}"
         response = requests.get(studies_url, headers=self.headers)
 
         if response.status_code == 200:
@@ -18,7 +22,7 @@ class Study:
             print(f"Failed to list studies. Status Code: {response.status_code}")
 
     def get_study_pgn(self, study_id):
-        study_url = f"{self.base_url}study/{study_id}.pgn"  
+        study_url = f"{self.base_url}study/{study_id}.pgn"
         response = requests.get(study_url, headers=self.headers)
 
         if response.status_code == 200:
@@ -27,8 +31,11 @@ class Study:
             print(f"Failed to retrieve PGN for study {study_id}. Status Code: {response.status_code}")
             return None
 
+# Example usage:
+study = Study()  # Initialize without a token
+# Authenticate later using a personal access token
 personal_token = "lip_EOEhmhXG2MsPWuBiDFI7"
-study = Study(personal_token)
+study.set_personal_token(personal_token)
 
 valid_username = "failedtofindaname"
 study.list_studies(valid_username)
@@ -36,4 +43,3 @@ study.list_studies(valid_username)
 valid_study_id = "OS94SFrY"
 pgn = study.get_study_pgn(valid_study_id)
 print(pgn)
-
