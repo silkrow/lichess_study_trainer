a6a33e4f5be6423c3a92a49bcec29aa87aa12bcf
Erkai Yu <si1krow@outlook.com>
2023-11-02 00:09:47 +0800

    a much more user friendly version

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ study_list_result = trainer.fetch_studies(valid_username)
 print(study_list_result)
 
 names = trainer.list_study_names()
-print(names)
 
-# trainer.set_crnt_study("[Repertoire] Accelerated Dragon")
-trainer.set_crnt_study("[Repertoire] Sicilian Sidelines")
+print(f"{valid_username} has following studies: ")
+for i in range(len(names)):
+    print(f"{i+1:3}: {names[i]}")
+
+number = input("Please input the index of the study you want to train on: ")
+
+trainer.set_crnt_study(names[int(number) - 1])
 
 trainer.training()
